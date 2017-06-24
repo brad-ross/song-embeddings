4f3bb1d595282ac1ee4ea1dd8fef3304658e86fb
Brad Ross <brad.ross.35@gmail.com>
2017-06-23 21:42:10 -0700

    init

---
+++ scrape_data/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end()
+})
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+  console.log('Press Ctrl+C to quit.')
+})
