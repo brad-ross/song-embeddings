40881b1b675d16b7e2b87d682498533eb69d0469
Brad Ross <brad.ross.35@gmail.com>
2017-11-10 08:45:28 -0800

    added a utility fn to cloud_storage.py for opening cloud storage files

--- code/data/cloud_storage.py
+++ code/data/cloud_storage.py
@@ -10,16 +10,22 @@ RETRY_DELAY = 2
 config = get_config()
 mount_location = config['storage']['mount_location']
 
+def get_path_to_bucket(bucket_name):
+    return os.path.expanduser(os.path.join(mount_location, bucket_name))
+
+def open_file_in_bucket(filename, bucket):
+    abs_path = get_path_to_bucket(bucket)
+    return open(os.path.join(abs_path, filename), 'r+') # should allow both reads and writes
+
 def save_raw_preview_to_cloud(raw_track, track_id, bucket_name):
-    abs_path = os.path.expanduser(os.path.join(mount_location, bucket_name))
     for i in range(NUM_RETRIES):
-	try:
-    	    with open(os.path.join(abs_path, '{0}.mp3').format(track_id), 'wb') as out_file:
+    	try:
+    	    with open_file_in_bucket('{0}.mp3'.format(track_id), bucket_name) as out_file:
             	shutil.copyfileobj(raw_track, out_file)
-	    return
-	except:
-	    print('ERROR: error when trying to save track {}'.format(track_id))
-	    
-        sleep(RETRY_DELAY)
+    	    return
+    	except:
+    	    print('ERROR: error when trying to save track {}'.format(track_id))
+    	    
+            sleep(RETRY_DELAY)
     
     print('ERROR: max retries exceeded when trying to save track {}'.format(track_id))
