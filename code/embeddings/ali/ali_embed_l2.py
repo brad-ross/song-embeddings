56edc6290cf2659281da6098f33789414667c5f4
Brad Ross <brad.ross.35@gmail.com>
2017-12-09 05:19:50 +0000

    got gan to train! added embedding function too!

---
+++ code/embeddings/ali/ali_embed_l2.py
@@ -0,0 +1,23 @@
+import numpy as np
+from ali_model_l2 import ALIModel
+from ...data.cloud_storage import open_file_in_bucket, get_path_to_file_in_bucket
+#from ali_train_l2 import rescale_batch
+
+def load_model_from(model_class, weights_file):
+    model = model_class()
+    model.load_weights_from_file(get_path_to_file_in_bucket(weights_file, 'song-embeddings-dataset'))
+    return model
+
+def create_embedding_fn(model_class, weights_file):
+    model = load_model_from(model_class, weights_file)
+    
+    def generate_embedding(specs):
+        specs_with_depth = specs[:, :, :, None]
+        return np.reshape(model.encoder.predict(specs_with_depth), (specs.shape[0], -1))
+
+    return generate_embedding
+
+embedding_fn = create_embedding_fn(ALIModel, 'model_weights_7_epoch_2')
+embedding = embedding_fn(np.load(open_file_in_bucket('test_0_1000.npy', 'song-embeddings-dataset')))
+print embedding
+print embedding.shape
