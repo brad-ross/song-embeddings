6c30dc3cae79b6ab9fdf7fcd1ce56c695c51cdc1
Prasanna Ramakrishnan <pras1712@stanford.du>
2017-12-09 20:47:05 -0800

    added bar plotting code, more test wrapping

---
+++ code/evaluation/bar_plotting.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bar_plot(dict1, dict2, categories, names, title, path):
+    plt.figure()
+    N = len(categories)
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35       # the width of the bars
+
+    scores1 = [dict1[c] for c in categories]
+    scores2 = [dict2[c] for c in categories]
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(ind, scores1, width, color='r')
+    rects2 = ax.bar(ind + width, scores2, width, color='g')
+
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(categories)
+    ax.set_ylim([0, 1])
+    ax.legend((rects1[0], rects2[0]), names)
+    plt.title(title)
+    plt.savefig(path)
+    # plt.show()
+
+def plot_for_task(results1, results2, metrics, names, path, task):
+    dict1 = {m:results1[task][m] for m in results1[task] if m in metrics}
+    dict2 = {m:results2[task][m] for m in results2[task] if m in metrics}
+    bar_plot(dict1, dict2, metrics, names, 'Performance on ' + task, path)
+
+def make_bar_plot(results1, results2, metrics, embed_names, path):
+    plot_for_task(results1, results2, metrics, embed_names, path + "kmeans.png", 'k-means Task')
+    plot_for_task(results1, results2, metrics, embed_names, path + "mog.png", 'Mixture of Gaussians Task')
--- code/evaluation/test_embeddings.py
+++ code/evaluation/test_embeddings.py
@@ -8,6 +8,7 @@ from ..embeddings.ali.ali_embed_l2 import create_embedding_fn
 from ..embeddings.ali.ali_model_l2 import ALIModel
 
 from plot_embeddings import *
+from bar_plotting import *
 
 LABEL_TO_GENRE = {
     0: 'classical',
@@ -45,9 +46,13 @@ GENRE_TO_LABEL = {
     'funk': 4
 }
 
+#things that are common that don't really need to be changed
+#(it was annoying to pass these around EVERYWHERE)
+ali_embedding = create_embedding_fn(ALIModel, 'model_weights_8_epoch_4')
+labels = get_numerical_labels(get_path_to_file_in_bucket('real_labels.csv', 'song-embeddings-dataset'))[108000:120000]
+specs = np.load(get_path_to_file_in_bucket('108000_120000.npy', 'song-embeddings-dataset'))
 
-def perform_tests(emebdding_fn, specs, labels, genres, num_per_genre,
-                fn_name="", plot=False, plot_path=None):
+def perform_tests(emebdding_fn, genres, num_per_genre, fn_name="", plot_path=None):
     chosen_genres = [GENRE_TO_LABEL[g] for g in genres]
     positions = np.array([], dtype=int)
     for cg in chosen_genres:
@@ -60,32 +65,85 @@ def perform_tests(emebdding_fn, specs, labels, genres, num_per_genre,
     results, embedding = gauntlet.run_tests(embedding_fn, new_specs, new_labels)
     print fn_name, 'results: '
     gauntlet.print_results(results)
-    if plot:
+    if plot_path != None:
         plot_embedding(embedding, new_labels, \
                        title='Embedding using ' + fn_name,\
-                       save_path=plot_path,\
+                       save_path=plot_path + '_embed_plot.png',\
                        label_dict=LABEL_TO_GENRE)
+    return results
+
+def run_comparison_tests(genres, num_per_genre, emebdding_fns=[pca_embedding, ali_embedding],\
+                         fn_names=['PCA Embedding', 'ALI Embedding'],\
+                         plot_path=None):
+    if plot_path != None:
+        paths = [plot_path + '_pca', plot_path + '_ali']
+    else:
+        paths = [None, None]
+
+    results_pca = perform_tests(emebdding_fns[0], genres, num_per_genre,\
+                                fn_name=fn_names[0], plot_path=paths[0])
+    results_ali = perform_tests(emebdding_fns[1], genres, num_per_genre,\
+                                fn_name=fn_names[1], plot_path=paths[1])
+
+    if plot_path != None:
+        make_bar_plot(results_pca, results_ali, ['Adjusted Mutual Information', 'V-measure'],\
+                      fn_names, plot_path + '_bar')
 
 
-labels = get_numerical_labels(get_path_to_file_in_bucket('real_labels.csv', 'song-embeddings-dataset'))[108000:120000]
-specs = np.load(get_path_to_file_in_bucket('108000_120000.npy', 'song-embeddings-dataset'))
+def do_all_tests_make_all_plots(genre_sets, nums_per_genre, test_names, plot_path=None):
+    for i in range(len(genre_sets)):
+        genres_to_run = genre_sets[i]
+        num_per_genre = nums_per_genre[i]
+        test_name = test_name[i]
+
+        curr_plot_path = plot_path
+        if plot_path != None:
+            curr_plot_path += '_' + test_name
+
+        run_comparison_tests(genres_to_run, num_per_genre, plot_path=curr_plot_path)
 
 
-genres_to_run = ['rock', 'indie rock', 'edm', 'classical']
-num_per_genre = 50
-embedding_fn  = pca_embedding
-fn_name       = "PCA Embedding"
-save_path     = get_path_to_file_in_bucket('pca_embedding_test_plot.png', 'song-embeddings-dataset')
-perform_tests(embedding_fn, specs, labels, genres_to_run, num_per_genre,\
-              fn_name=fn_name, plot=True, plot_path=save_path)
+test_names = [
+    'three_diff',
+    'two_sim_one_diff'
+    'all_genres'
+]
 
-genres_to_run = ['rock', 'indie rock', 'edm', 'classical']
-num_per_genre = 50
-embedding_fn  = create_embedding_fn(ALIModel, 'model_weights_8_epoch_4')
-fn_name       = "ALI Embedding"
-save_path     = get_path_to_file_in_bucket('ali_embedding_test_plot.png', 'song-embeddings-dataset')
-perform_tests(embedding_fn, specs, labels, genres_to_run, num_per_genre,\
-              fn_name=fn_name, plot=True, plot_path=save_path)
+genre_sets = [
+    ['rock', 'edm', 'classical'],
+    ['rock', 'indie rock', 'classical'],
+    GENRE_TO_LABEL.keys()
+]
+
+nums_per_genre = [
+    50,
+    50,
+    50
+]
+save_path     = get_path_to_file_in_bucket('plotting_test', 'song-embeddings-dataset')
+
+do_all_tests_make_all_plots(genre_sets, nums_per_genre, test_names, plot_path=save_path)
+
+#just one test:
+# genres_to_run = ['rock', 'indie rock', 'edm', 'classical']
+# num_per_genre = 50
+# save_path     = get_path_to_file_in_bucket('plotting_test', 'song-embeddings-dataset')
+#
+# run_comparison_tests(specs, labels, genres_to_run, num_per_genre, plot_path=save_path)
+
+#
+# embedding_fn  = pca_embedding
+# fn_name       = "PCA Embedding"
+# perform_tests(embedding_fn, specs, labels, genres_to_run, num_per_genre,\
+#               fn_name=fn_name, plot=True, plot_path=save_path)
+#
+# genres_to_run = ['rock', 'indie rock', 'edm', 'classical']
+# num_per_genre = 50
+# embedding_fn  = create_embedding_fn(ALIModel, 'model_weights_8_epoch_4')
+# fn_name       = "ALI Embedding"
+# save_path     = get_path_to_file_in_bucket('ali_embedding_test_plot.png', 'song-embeddings-dataset')
+# perform_tests(embedding_fn, specs, labels, genres_to_run, num_per_genre,\
+#               fn_name=fn_name, plot=True, plot_path=save_path)
 #
 # small_indices = np.where((labels == 2) | (labels == 0) | (labels == 14))
 # labels = labels[small_indices]
