aa7fffea67675b0969f8b9819e6c04e10d2bae01
Prasanna Ramakrishnan <pras1712@stanford.du>
2017-12-09 23:11:23 -0800

    merged bar plots

--- code/evaluation/bar_plotting.py
+++ code/evaluation/bar_plotting.py
@@ -2,8 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def bar_plot(dict1, dict2, categories, names, title, path):
+def bar_plot(dict1, dict2, names, title, path):
     plt.figure()
+#     categories = dict1.keys()
+    categories = [
+        'k-means:\nV-measure',
+        'k-means:\nAdjusted Mutual Information',
+        'Mixture of Gaussians:\nV-measure',
+        'Mixture of Gaussians:\nAdjusted Mutual Information',
+    ]
+    print categories
     N = len(categories)
     ind = np.arange(N)  # the x locations for the groups
     width = 0.35       # the width of the bars
@@ -11,7 +19,8 @@ def bar_plot(dict1, dict2, categories, names, title, path):
     scores1 = [dict1[c] for c in categories]
     scores2 = [dict2[c] for c in categories]
 
-    fig, ax = plt.subplots()
+    fig = plt.figure(figsize=(10, 2))
+    ax = fig.add_subplot(111)
     rects1 = ax.bar(ind, scores1, width, color='r')
     rects2 = ax.bar(ind + width, scores2, width, color='g')
 
@@ -21,13 +30,17 @@ def bar_plot(dict1, dict2, categories, names, title, path):
     ax.legend((rects1[0], rects2[0]), names)
     plt.title(title)
     plt.savefig(path)
-    # plt.show()
+    plt.show()
 
-def plot_for_task(results1, results2, metrics, names, path, task):
-    dict1 = {m:results1[task][m] for m in results1[task] if m in metrics}
-    dict2 = {m:results2[task][m] for m in results2[task] if m in metrics}
-    bar_plot(dict1, dict2, metrics, names, 'Performance on ' + task, path)
+def merge_results(results, metrics):
+    def fix_task(m):
+        return ' '.join(m.split(' ')[:-1])
+    return {fix_task(task) + ':\n' + metric:results[task][metric] for metric in metrics for task in results}
+
+
 
 def make_bar_plot(results1, results2, metrics, embed_names, path):
-    plot_for_task(results1, results2, metrics, embed_names, path + '_kmeans.png', 'k-means Task')
-    plot_for_task(results1, results2, metrics, embed_names, path + '_mog.png', 'Mixture of Gaussians Task')
+    m_results1 = merge_results(results1, metrics)
+    print m_results1
+    m_results2 = merge_results(results2, metrics)
+    bar_plot(m_results1, m_results2, embed_names, 'Performance on k-means and Mixture of Gaussians', path + '.png')
